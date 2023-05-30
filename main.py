5a0f4daefe2e6467743121d717584378dfe4a236
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-05-29 22:25:10 -0400

    BMI eq

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ print("Welcome to the Body Mass Index (BMI) Calculator\n")
 height = float(input("enter your height in m: "))
 weight = float(input("enter your weight in kg: "))
 
+BMI = weight/(height**2)
