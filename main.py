08e85dfa4959419ec6b02aa1f7ffd1fc4dbe6c63
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-05-29 23:12:48 -0400

    added system option

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 print("Welcome to the Body Mass Index (BMI) Calculator\n")
 
-height = float(input("enter your height in m: "))
-weight = float(input("enter your weight in kg: "))
+system = input("Which system would you like to use?\n Type 'i' for Imperial (pounds/ inches) or 'm' for metric: ")
 
-BMI = round(weight/(height**2))
 
-if BMI > 18.5:
+if system == "m" or system == "M":
+  height = float(input("enter your height in m: "))
+  weight = float(input("enter your weight in kg: "))
+  BMI = round(weight/(height**2))
+  if BMI > 18.5:
     if BMI <= 35:
         if BMI <= 30:
             if BMI <= 25:
@@ -15,5 +17,23 @@ if BMI > 18.5:
             print(f"Your BMI is {BMI}, you are obese.")
     else:
         print(f"Your BMI is {BMI}, you are clinicaly obese.")
+  else:
+    print(f"Your BMI is {BMI}, you are underweight.")
+elif system == "i" or system == "I":
+  height = float(input("enter your height in inches: "))
+  weight = float(input("enter your weight in lbs: "))
+  BMI = round((weight/(height**2))*703)
+  if BMI > 18.5:
+    if BMI <= 35:
+        if BMI <= 30:
+            if BMI <= 25:
+              print(f"Your BMI is {BMI}, you have a normal weight.")
+            else: print(f"Your BMI is {BMI}, you are slightly overweight.")  
+        else:
+            print(f"Your BMI is {BMI}, you are obese.")
+    else:
+        print(f"Your BMI is {BMI}, you are clinicaly obese.")
+  else:
+    print(f"Your BMI is {BMI}, you are underweight.")
 else:
-    print(f"Your BMI is {BMI}, you are underweight.")                    
+   system = input("Please enter either 'i' for Imperial or 'm' for Metric")                    
