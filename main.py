75ed56501f5e02d4bfe4cfb7c50dde68ac04ff2f
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-05-29 22:49:19 -0400

    changed results to f strings

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ if BMI > 18.5:
     if BMI <= 35:
         if BMI <= 30:
             if BMI <= 25:
-              print("Your BMI is " + BMI + ", you have a normal weight.")
-            else: print("Your BMI is " + BMI + ", you are slightly overweight.")  
+              print(f"Your BMI is {BMI}, you have a normal weight.")
+            else: print(f"Your BMI is {BMI}, you are slightly overweight.")  
         else:
-            print("Your BMI is " + BMI + ", you are obese.")
+            print(f"Your BMI is {BMI}, you are obese.")
     else:
-        print("Your BMI is " + BMI + ", you are clinicaly obese.")
+        print(f"Your BMI is {BMI}, you are clinicaly obese.")
 else:
-    print("Your BMI is " + BMI + ", you are underweight.")                    
+    print(f"Your BMI is {BMI}, you are underweight.")                    
