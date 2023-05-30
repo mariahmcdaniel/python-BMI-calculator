8d22ed6739c1c2ec94be96e94e3942e932fca537
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-05-29 22:42:29 -0400

    if/else statements

--- main.py
+++ main.py
@@ -4,3 +4,16 @@ height = float(input("enter your height in m: "))
 weight = float(input("enter your weight in kg: "))
 
 BMI = round(weight/(height**2))
+
+if BMI > 18.5:
+    if BMI <= 35:
+        if BMI <= 30:
+            if BMI <= 25:
+              print("Your BMI is " + BMI + ", you have a normal weight.")
+            else: print("Your BMI is " + BMI + ", you are slightly overweight.")  
+        else:
+            print("Your BMI is " + BMI + ", you are obese.")
+    else:
+        print("Your BMI is " + BMI + ", you are clinicaly obese.")
+else:
+    print("Your BMI is " + BMI + ", you are underweight.")                    
